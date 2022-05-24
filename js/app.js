752f29c2b67cb1ff1469b8f01b698052e8ec5b75
justinburgess <signalincode@gmail.com>
2022-05-24 12:47:09 -0400

    resolved next/prev, birthday format bugs

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // selectors
 const mainSelector = document.querySelector('main');
 const searchSelector = document.getElementById('search');
-let switchSelector;
+const overlaySelector = document.getElementById('overlay');
 
 // stores current page data -- does not store session data
 let employeeData;
@@ -19,8 +19,7 @@ fetchEmployee(12);
 */
 
 // obtains employee data from API, stores data and creates employee cards in the DOM
-
-function fetchEmployee(employeeCount) {
+function fetchEmployee(employeeCount){
      fetch(`https://randomuser.me/api/?results=${employeeCount}&inc=name,email,location,cell,dob,picture`)
           .then(response => response.json())
           .then(data => createEmployeesObject(data.results))
@@ -28,19 +27,21 @@ function fetchEmployee(employeeCount) {
           .catch(error => console.log('Woops! Looks like there was a problem.', error));
 }
 
-// creates and stores object data
-
-function createEmployeesObject(data) {
+// creates and stores object data for current page
+function createEmployeesObject(data){
      let employees = [];
      for(let i = 0; i < data.length; i++) {
           const birthday = new Date(data[i].dob.date);
+          const birthMonth = birthday.getMonth().toString().length === 1 ? '0' + birthday.getMonth().toString() : birthday.getMonth().toString();
+          const birthDay = birthday.getDay().toString().length === 1 ? '0' + birthday.getDay().toString() : birthday.getDay().toString();
+          const birthYear = birthday.getFullYear().toString().slice(2,4);
           const employee = {
                name: `${data[i].name.first} ${data[i].name.last}`,
                email: data[i].email,
                phone: data[i].cell,
                city: data[i].location.city,
                address: `${data[i].location.street.number} ${data[i].location.street.name} ${data[i].location.city}, ${data[i].location.state} ${data[i].location.postcode}`,
-               birthday: `${birthday.getMonth()}/${birthday.getDay()}/${birthday.getFullYear().toString().slice(2,4)}`,
+               birthday: `${birthMonth}/${birthDay}/${birthYear}`,
                picture: data[i].picture.large
           };
           employees.push(employee);
@@ -50,7 +51,7 @@ function createEmployeesObject(data) {
 }
 
 // creates card html
-function createEmployeeHTML(employee) {
+function createEmployeeHtml(employee){
      let employeeHTML = `
                <div class="card" tabindex="0">
                     <img src="${employee.picture}" alt="${employee.name}, an employee of awesome startup">
@@ -65,16 +66,16 @@ function createEmployeeHTML(employee) {
 }
 
 // updates DOM with card html
-function createEmployeeCards(employees) {
+function createEmployeeCards(employees){
      html = '';
      employees.forEach(employee => {
-          html += createEmployeeHTML(employee);
+          html += createEmployeeHtml(employee);
      });
-     mainSelector.innerHTML = html;
+     mainSelector.insertAdjacentHTML('beforeend', html);
 }
 
 // returns employee data based on employee name
-function getEmployeeData(employeeName) {
+function getEmployeeData(employeeName){
      for (let i = 0; i < employeeData.length; i++){
           if(employeeData[i].name === employeeName){
                return employeeData[i];
@@ -82,18 +83,9 @@ function getEmployeeData(employeeName) {
      }
 }
 
-// deletes current employee overlay modal window
-function checkOverlayExists(){
-     firstElement = mainSelector.firstElementChild;
-     if(firstElement.id === 'overlay'){
-          firstElement.remove();
-     };
-}
-
 // creates html for employee modal window and inserts into DOM
-function createEmployeeOverlay(employee){
+function createEmployeeOverlayHtml(employee){
      let employeeHTML = `
-     <div id="overlay">
           <div id="lightbox-card">
                <p class="close">X</p>
                <img src="${employee.picture}" alt="${employee.name}, an employee of awesome startup">
@@ -105,63 +97,71 @@ function createEmployeeOverlay(employee){
                     <p>${employee.address}</p>
                     <p>${employee.birthday}</p>
                     <div class=lightbox-switch-employee>
-                         <p class="back">&lt; Previous</p>
+                         <p class="backward">&lt; Previous</p>
                          <p class="forward">Next &gt;</p>
                     </div>
                </div>
           </div>
-     </div>
+     
      `
-     mainSelector.insertAdjacentHTML("afterbegin",employeeHTML);
+     return employeeHTML
 }
 
-// switches to a new employee and recursively resets modal window when modal selection changed
-function setEventListener(employee){
-     switchSelector.addEventListener('click', (e) => {
-          target = e.target;
-          const employeeIndex = employeeData.indexOf(employee);
-          console.log(employeeIndex);
-          if(target.className === 'forward' && employeeIndex < 11) {
-               checkOverlayExists();
-               createEmployeeOverlay(employeeData[employeeIndex + 1])
-          }
-          if(target.className === 'back' && employeeIndex > 0) {
-               checkOverlayExists();
-               createEmployeeOverlay(employeeData[employeeIndex - 1])
-               
-          }
-     switchSelector = document.querySelector('.lightbox-switch-employee');
-     const nextEmployee = getEmployeeData(document.getElementById('name').textContent)
-     setEventListener(nextEmployee);
-     });
+function openModalWindow(target){
+     const employeeName = target.firstElementChild.nextElementSibling.firstElementChild.textContent;
+     const employee = getEmployeeData(employeeName);
+     const employeeOverlayHtml = createEmployeeOverlayHtml(employee);
+     if(overlaySelector.firstElementChild){
+          overlaySelector.firstElementChild.remove();
+     }
+     overlaySelector.insertAdjacentHTML('afterbegin', employeeOverlayHtml);
+     if(employeeData.indexOf(employee) === 11){
+          document.querySelector('.forward').remove();
+     } else if(employeeData.indexOf(employee) === 0){
+          document.querySelector('.backward').remove();
+     }
+     overlaySelector.style.display = '';
 }
 
-/*
-*    listeners
-*/
+function switchEmployeeOverlay(className){
+     const employeeName = overlaySelector.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.textContent;
+     const employee = getEmployeeData(employeeName);
+     const nextEmployeeIndex =  className === 'forward' ? employeeData.indexOf(employee) + 1 : employeeData.indexOf(employee) - 1;
+     const employeeOverlayHtml = createEmployeeOverlayHtml(employeeData[nextEmployeeIndex]);
+     overlaySelector.firstElementChild.remove();
+     overlaySelector.insertAdjacentHTML('afterbegin', employeeOverlayHtml);
+     if(nextEmployeeIndex >= 11){
+          document.querySelector('.forward').remove();
+     } else if(nextEmployeeIndex <= 0){
+          document.querySelector('.backward').remove();
+     }
+}
 
-// changes modal window employee selection by focus
-document.addEventListener( 'focus', (e) => {
-     target = e.target;
+// /*
+// *    listeners
+// */
+
+//
+document.addEventListener('focus', (e) => {
+     const target = e.target;
      if(target.className === 'card'){
-          const employeeName = target.firstElementChild.nextElementSibling.firstElementChild.textContent;
-          const employee = getEmployeeData(employeeName);
-          
-          // check for existing overlay
-          checkOverlayExists();
-          // create employee overlay
-          createEmployeeOverlay(employee);
-          // set selector for overlay employee switch
-          switchSelector = document.querySelector('.lightbox-switch-employee');
-          setEventListener(employee);
+          openModalWindow(target)
      }
 }, true);
+
+overlaySelector.addEventListener('click', (e) => {
+     const target = e.target;
+     if(target.className === 'forward' || target.className === 'backward'){
+          switchEmployeeOverlay(target.className);
+     }
+});
 
 // closes modal window when 'X' is clicked
 document.addEventListener('click', (e) => {
      target = e.target;
      if(target.className === 'close'){
-          mainSelector.firstElementChild.remove();
+          overlaySelector.firstElementChild.remove();
+          overlaySelector.style.display = 'none';
      }
 });
 
